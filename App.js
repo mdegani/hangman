82de25d8ee906e91318985dbedbad57896c75644
Mark Degani <mark.degani@outlook.com>
2015-04-22 22:18:51 -0400

    Rebrand.  Added bootstrap.

--- App.js
+++ App.js
@@ -2,9 +2,6 @@
 
 var hangman = (function () {
 
-    var hangedGuy = "__<br/>|  | <br/> o <br/>-|- <br/> /\\"
-    document.getElementById("hangedGuy").innerHTML = hangedGuy;
-
     var allLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     //this array will track the status of all of the letter
     //of the alphabet:
